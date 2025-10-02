2b3052ea18fd686764abaeeb44b52d1c5cef18df
shewit <hailu.shewit47@gmail.com>
2025-10-01 21:43:57 -0400

    finalized filed

--- src/main/java/com/pluralsight/HighScore.java
+++ src/main/java/com/pluralsight/HighScore.java
@@ -1,42 +1,42 @@
 package com.pluralsight;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class HighScore {
     public static void main(String[] args) {
         String format = "Home:Visitor|21:9";
 
         String[] name = format.split("\\|");
 
-        String section1 = name[0];
-        String section2 = name[1];
+        String sideLeft = name[0];
+        String sideRight = name[1];
 
-        System.out.println("Part 1 ");
-        System.out.println("Original- " + format);
-        System.out.println(section1);
-        System.out.println(section2);
-
+        System.out.println("------Part 1------");
+        System.out.println("Original: " + format);
+        System.out.println(sideLeft);
+        System.out.println(sideRight);
         System.out.println();
-        System.out.println("Part 2 ");
 
-        String[] name2 = section1.split("\\:");
-        String part1 = name2[0];
-        String part2 = name2[1];
-
-        System.out.println(section1);
-        System.out.println(part1);
-        System.out.println(part2);
-
-        String[] name3 = section2.split("\\:");
-
-        String door1 = name3[0];
-        String door2 = name3[1];
-
+        System.out.println("------Part 2------");
+        String[] leftSideSemi = sideLeft.split("\\:");
+        String leftSide = leftSideSemi[0];
+        String rightSide = leftSideSemi[1];
+        System.out.println("Original: " + sideLeft);
+        System.out.println(leftSide);
+        System.out.println(rightSide);
         System.out.println();
-        System.out.println("Part 3: ");
-        System.out.println(section2);
-        System.out.println(door1);
-        System.out.println(door2);
+
+        System.out.println("------Part 3------");
+        String[] rightSideSemi = sideRight.split("\\:");
+        String left_side = rightSideSemi[0];
+        String right_side = rightSideSemi[1];
+        System.out.println("Original: " + sideRight);
+        System.out.println(left_side);
+        System.out.println(right_side);
+        System.out.println();
+
+
+
+
+
 
 
 
